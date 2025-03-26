e01bb1741ccab60e1e1ecf2095a001afba5c047e
simay-uygur <simayuygur09@gmail.com>
2025-03-26 16:56:11 +0300

    Revert "import of basic structure of the project"
    This reverts commit 1646d503832a9afa8813c0e39428a46718c538db.

--- demo1/src/main/java/com/example/demo1/exception/UserExistsExc.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo1.exception;
-
-public class UserExistsExc extends RuntimeException{
-    public UserExistsExc(Long id){
-        super("User with " + id + " already exists.") ;
-    }
-}
