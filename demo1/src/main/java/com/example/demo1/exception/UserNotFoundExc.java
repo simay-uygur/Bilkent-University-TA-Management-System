98f07cc1183668025d4385ee9d4c4a1d29c4b981
simay-uygur <simayuygur09@gmail.com>
2025-03-28 00:00:20 +0300

    docker configuration added

--- demo1/src/main/java/com/example/demo1/exception/UserNotFoundExc.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo1.exception;
-
-public class UserNotFoundExc extends RuntimeException{
-    public UserNotFoundExc(Long id){
-        super("User with " + id + " not found in the DB.") ;
-    }
-}
