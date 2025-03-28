a4eccbcb4141c0f501eec688c67b3c325ee52c41
Perhat Amanlyyev <116658502+PEPEZHK@users.noreply.github.com>
2025-03-28 14:01:23 +0300

    Update of classes in the project
    bug fixes. change abstract user to class user. implementation of task sevice, ta serv and etc

--- backend/src/main/java/com/example/entity/Task.java
+++ backend/src/main/java/com/example/entity/Task.java
@@ -1,6 +1,9 @@
 package com.example.entity;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -11,39 +14,42 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+//@Table(name = "task_table", uniqueConstraints = @UniqueConstraint(columnNames = {"course_id", "task_id"}))
+@Table(name = "task_table")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto id generation
+    @Column(name = "task_id", unique = true, updatable = false, nullable = false)
     private int task_id;
 
-    @ManyToOne // when using relationships needs to know who // changes did not save in visual paradigm ))))))
+    /*@ManyToOne // when using relationships needs to know who // changes did not save in visual paradigm ))))))
     @JoinColumn(name = "course_id")
-    private Course course; //many to one
+    private Course course; //many to one*/
 
     @ManyToMany(mappedBy="ta_tasks_list",fetch = FetchType.LAZY) // mappedby means that the other side is the owner of the relationship(TA)
-    private List<TA> tas_list ; //many to many
+    @JsonIgnore
+    private Set<TA> tas_list = new HashSet<TA>(); //many to many
 
-    @ManyToMany(mappedBy="section_tasks_list",fetch = FetchType.LAZY)
-    private List<Section> sections_list ; //many to many
-
-    @Column(name = "is_approved", unique = false, updatable = true, nullable = false)
-    private boolean isApproved;
+    /*@ManyToMany(mappedBy="section_tasks_list",fetch = FetchType.LAZY)
+    private List<Section> sections_list ; //many to many*/
     
     @Embedded
+    @Column(name = "duration", unique = false, updatable = true, nullable = false)
     private Event duration;
 
-    public boolean isTaskActive(Date currentDate) {
-        return duration.isOngoing(currentDate);
-    }
+    @Column(name = "required_tas", unique = false, updatable = true, nullable = false)
+    private int requiredTAs;
+
+    @Column(name = "size_of_tas", unique = false, updatable = true, nullable = false)
+    private int amount_of_tas;
 
     @Column(name = "is_time_passed", unique = false, updatable = true, nullable = false)
     private boolean isTimePassed;
@@ -57,16 +63,13 @@ public class Task {
     @Column(name = "type", unique = false, updatable = true, nullable = false)
     @Enumerated(EnumType.STRING)
     private TaskType type;
+    
+    @Column(name = "status", unique = false, updatable = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TaskState status;
 
-    private void mark_approved() {
-        this.isApproved = true;
-        for (TA ta : tas_list) {
-            ta.getTa_tasks_list().remove(this);
-        }
-        //tas_list.clear();
-    }
-
-    public void approve(){
-        mark_approved();
+    //Method to check if task is still active
+    public boolean isTaskActive() {
+        return duration.isOngoing();
     }
 }
