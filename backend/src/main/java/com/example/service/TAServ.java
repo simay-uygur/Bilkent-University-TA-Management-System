34335ca3c105f9626532c7e20d1f60e1afb0c0f1
Perhat Amanlyyev <116658502+PEPEZHK@users.noreply.github.com>
2025-03-28 14:01:23 +0300

    Update of classes in the project
    bug fixes. change abstract user to class user. implementation of task sevice, ta serv and etc

--- backend/src/main/java/com/example/service/TAServ.java
+++ backend/src/main/java/com/example/service/TAServ.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Set;
 
 import com.example.entity.TA;
 import com.example.entity.Task;
@@ -9,8 +10,9 @@ public interface TAServ {
     public List<TA> getAllTAs();
     public boolean deleteTAById(Long id);
     public TA getTAById(Long id);
-    public Task getTaskById(int id);
-    public List<Task> getAllTasks(Long id);
-    public Task createTask(Task task);
-    public boolean deleteTaskById(int id);
+    public Task getTaskById(int task_id, Long ta_id);
+    public Set<Task> getAllTasks(Long id);
+    public Task assignTask(Task task, Long id);
+    public boolean deleteTaskById(int task_id, Long ta_id);
+    public boolean restoreTAById(Long id);
 }
