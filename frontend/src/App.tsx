c233650c8be8c1c7ac5a5d27c7c3eb677f0f9ac5
Anıl Keskin <anil04keskin@gmail.com>
2025-04-17 20:59:35 +0300

    LeaveRequest of TA
    I hope so

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import LoginPage from './components/LoginPage';
 import AdminDashboard from './components/AdminDashboard.tsx';
 import InstructorDashboard from './components/InstructorDashboard.tsx';
 import TADashboard from './components/TADashboard.tsx';
-
+import LeaveRequestForm from './components/LeaveRequestForm.tsx';
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
@@ -14,6 +14,10 @@ const App: React.FC = () => (
       <Route path="/instructor" element={<InstructorDashboard />} />
       <Route path="/dashboard" element={<TADashboard />} />
       {/* add a catch‑all 404 if you like */}
+      <Route
+    path="/leave-request/:scheduleId"
+    element={<LeaveRequestForm />}
+  />
     </Routes>
   </BrowserRouter>
 );
