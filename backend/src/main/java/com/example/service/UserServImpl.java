d6dbfd26e5da4dfde6051e452586792ba4c5e12d
Perhat Amanlyyev <116658502+PEPEZHK@users.noreply.github.com>
2025-05-01 15:42:15 +0300

    authcontroller

--- backend/src/main/java/com/example/service/UserServImpl.java
+++ backend/src/main/java/com/example/service/UserServImpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Actors.TA;
@@ -28,8 +28,8 @@ public class UserServImpl implements UserServ{
     @Autowired
     private TARepo taRepo;
 
-    @Autowired
-    private final BCryptPasswordEncoder encoder ;
+    private final PasswordEncoder encoder;
+
     
     @Override
     public List<User> getAllUsers(){
