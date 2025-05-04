7ebc71cfdd9610d4c44e106aae680a121ff1f64a
Perhat Amanlyyev <116658502+PEPEZHK@users.noreply.github.com>
2025-05-04 17:27:48 +0300

    continuation

---
+++ backend/src/main/java/com/example/RequestRepos/SwapEnableRepo.java
@@ -0,0 +1,12 @@
+package com.example.repo.RequestRepos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Requests.SwapEnable;
+
+@Repository
+public interface SwapEnableRepo extends JpaRepository<SwapEnable, Long> {
+    boolean existsBySenderIdAndReceiverId(Long senderId, Long receiverId);
+    boolean existsBySenderIdAndReceiverIdAndExamIdAndIsRejected(Long id, Long receiverId, int examId, boolean isRejected);
+}
